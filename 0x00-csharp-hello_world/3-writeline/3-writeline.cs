1818f6eb4b40e1ca9ccf8480d375c2077265df05
sebastianchc <1200@holbertonschool.com>
2020-12-01 00:02:35 -0500

    Reinstalling .NET from 5.0 to 2.1 version

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,1 +1,12 @@
-﻿System.Console.WriteLine("\"Programming is like building a multilingual puzzle");
+﻿using System;
+
+namespace _3_writeline
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"Programming is like building a multilingual puzzle");
+        }
+    }
+}
