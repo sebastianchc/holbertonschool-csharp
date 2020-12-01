4db1bd3006f7e7e586b9531e51ffb88636a3d1ca
sebastianchc <1200@holbertonschool.com>
2020-11-30 23:16:27 -0500

    Simple code

--- 0x00-csharp-hello_world/3-writeline/3-writeline.cs
+++ 0x00-csharp-hello_world/3-writeline/3-writeline.cs
@@ -1,12 +1,2 @@
 ﻿using System;
-
-namespace _3_writeline
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("\"Programming is like building a multilingual puzzle");
-        }
-    }
-}
+Console.WriteLine("\"Programming is like building a multilingual puzzle");
