c418fcbd9064d768983a82efe718ba4e913c1d2a
sebastianchc <1200@holbertonschool.com>
2020-12-02 10:42:27 -0500

    99 Comb part II

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+	    int j;
+            for (i = 0; i < 9; i++)
+	    	for (j = 1; j < 10; j++)
+		    if (j > i)
+		       if (i == 8 && j == 9)
+		       	  Console.WriteLine("{0}{1}", i, j);
+		       else
+			  Console.Write("{0}{1}, ", i, j);
+        }
+    }
+}
