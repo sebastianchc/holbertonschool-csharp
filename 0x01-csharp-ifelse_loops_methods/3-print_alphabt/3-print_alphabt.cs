671f5006c1671da9c39e2f9f9e74f15c9e7a33c6
sebastianchc <1200@holbertonschool.com>
2020-12-02 09:46:25 -0500

    Alphabet soup

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char i;
+	    for (i = 'a'; i <= 'z'; i++)
+	    	if (i != 'e' && i != 'q')
+		   Console.Write(i);
+        }
+    }
+}
