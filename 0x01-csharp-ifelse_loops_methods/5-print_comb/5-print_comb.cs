e1f66cd2d1e90f015143010108f085145483c5f7
sebastianchc <1200@holbertonschool.com>
2020-12-02 10:14:32 -0500

    00...99 comb

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 100; i++)
+	    	if (i == 99)
+	    	   Console.WriteLine(i);
+		else
+		   Console.Write("{0:D2}, ", i);
+        }
+    }
+}
