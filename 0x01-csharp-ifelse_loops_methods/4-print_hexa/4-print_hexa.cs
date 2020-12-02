26cebcaf7ffb4444acaf1fca9dad374277adbd38
sebastianchc <1200@holbertonschool.com>
2020-12-02 09:59:41 -0500

    Hexadecimal printing

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 99; i++)
+	    	Console.WriteLine("{0} = 0x{0:x}", i);
+        }
+    }
+}
