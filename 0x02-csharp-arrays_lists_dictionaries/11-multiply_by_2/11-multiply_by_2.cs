391490a079eb189295aa570925b76ae175338a97
sebastianchc <1200@holbertonschool.com>
2020-12-02 23:48:35 -0500

    Loop a dictionary

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+	{
+		Dictionary<string, int> newDict = new Dictionary<string, int>();
+		foreach(var i in myDict.Keys)
+		{
+			newDict[i] = myDict[i] * 2;
+		}
+		return(newDict);
+	}
+}
