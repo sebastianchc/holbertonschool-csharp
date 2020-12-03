2101622b95ea8c880ed715ce39f0d0d93abba23d
sebastianchc <1200@holbertonschool.com>
2020-12-02 19:26:30 -0500

    Replace array element

---
+++ 0x02-csharp-arrays_lists_dictionaries/2-replace_element/2-replace_element.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Array
+{
+	public static int[] ReplaceElement(int[] array, int index, int n)
+	{
+		if (index >= array.Length || index < 0)
+	        {
+                        Console.WriteLine("Index out of range");
+		}
+		else
+		{
+			array[index] = n;
+		}
+		return(array);
+	}
+}
