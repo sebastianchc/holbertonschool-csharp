af47893cad1b2571ce2c989989d4b6172e150c5f
sebastianchc <1200@holbertonschool.com>
2020-12-02 23:03:38 -0500

    Remove list item

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static List<int> DeleteAt(List<int> myList, int index)
+	{
+		if (index >= myList.Count || index < 0)
+		{
+			Console.WriteLine("Index is out of range");
+		}
+		else
+		{
+			myList.Remove(myList[index]);
+		}
+		return(myList);
+	}
+}
