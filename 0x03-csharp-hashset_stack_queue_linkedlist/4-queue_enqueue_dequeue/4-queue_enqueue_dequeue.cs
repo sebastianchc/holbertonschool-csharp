7e761867862cbf2488503bdbf044604f29e0b857
sebastianchc <1200@holbertonschool.com>
2020-12-06 12:22:39 -0500

    Introducing to Queues

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+	public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+	{
+		string ward;
+		Console.WriteLine("Number of items: {0}", aQueue.Count);
+		if (aQueue.Count == 0)
+		{
+			Console.WriteLine("Queue is empty");
+		}
+		else
+		{
+			Console.WriteLine("Top item: {0}", aQueue.Peek());
+		}
+		aQueue.Enqueue(newItem);
+		if (aQueue.Contains(search))
+		{
+			Console.WriteLine("Queue contains \"{0}\": True", search);
+			while (aQueue.Count > 0)
+			{
+				ward = aQueue.Peek();
+				aQueue.Dequeue();
+				if (ward == search)
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			Console.WriteLine("Queue contains \"{0}\": False", search);
+		}
+		return aQueue;
+	}
+}
