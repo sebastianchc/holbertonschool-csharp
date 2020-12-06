97e55f75b0342e5313b7fad0370f0c302147b59f
sebastianchc <1200@holbertonschool.com>
2020-12-06 11:57:37 -0500

    Introducing to stacks

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+	public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+	{
+		string ward;
+		Console.WriteLine("Number of items: {0}", aStack.Count);
+		Console.WriteLine("Top item: {0}", aStack.Peek());
+		if (aStack.Contains(search))
+		{
+			Console.WriteLine("Stack contains \"{0}\": True", search);
+			while (aStack.Count > 0)
+			{
+				ward = aStack.Peek();
+				aStack.Pop();
+				if (ward == search)
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			Console.WriteLine("Stack contains \"{0}\": False", search);
+		}
+		aStack.Push(newItem);
+		return aStack;
+	}
+}
