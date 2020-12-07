608f5db23f27bdddb266aeb60e99af8ba6fbd1d0
sebastianchc <1200@holbertonschool.com>
2020-12-06 23:33:40 -0500

    Creating a matrix

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		int[,] matrix = new int[5,5];
+		for (int i = 0; i < 5; i++)
+		{
+			for (int j = 0; j < 5; j++)
+			{
+				if (i == 2 && j == 2)
+				{
+					matrix[i, j] = 1;
+				}
+				else
+				{
+					matrix[i, j] = 0;
+				}
+				if (j == 4)
+				{
+					Console.WriteLine("{0}", matrix[i, j]);
+				}
+				else
+				{
+					Console.Write("{0} ", matrix[i, j]);
+				}
+			}
+		}
+        }
+    }
+}
