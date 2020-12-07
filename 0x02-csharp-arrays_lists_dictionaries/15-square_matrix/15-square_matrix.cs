8a0f715c5f918aa5fd76974b7a3bba57b1f52fe4
sebastianchc <1200@holbertonschool.com>
2020-12-06 23:46:43 -0500

    Learning .GetLength method

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+class Matrix
+{
+	public static int[,] Square(int[,] myMatrix)
+	{
+		int[,] matrix = new int[myMatrix.GetLength(0),myMatrix.GetLength(1)];
+		for (int i = 0; i < myMatrix.GetLength(0); i++)
+		{
+			for (int j = 0; j < myMatrix.GetLength(1); j++)
+			{
+				matrix[i, j] = myMatrix[i, j] * myMatrix[i, j];
+			}
+		}
+		return matrix;
+	}
+}
