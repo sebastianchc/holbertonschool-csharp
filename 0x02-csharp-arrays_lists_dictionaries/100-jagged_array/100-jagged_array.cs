6159cc1769939a68d821f7c4eeab438e2d31b482
sebastianchc <1200@holbertonschool.com>
2020-12-06 23:58:20 -0500

    Strange jagged array

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		int[][] jaggedArray = new int[3][];
+		jaggedArray[0] = new int[] { 0, 1, 2, 3 };
+		jaggedArray[1] = new int[] { 0, 1, 2, 3, 4, 5, 6 };
+		jaggedArray[2] = new int[] { 0, 1 };
+		for (int i = 0; i < jaggedArray.Length; i++)
+		{
+			for (int j = 0; j < jaggedArray[i].Length; j++)
+			{
+				Console.Write(jaggedArray[i][j]);
+				if (j != jaggedArray[i].Length - 1)
+				{
+					Console.Write(' ');
+				}
+			}
+			Console.WriteLine();
+		}
+        }
+    }
+}
