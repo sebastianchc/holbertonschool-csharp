389ed02026e3ccb803cb30e0cd3a0dc3b168f418
sebastianchc <1200@holbertonschool.com>
2020-12-06 19:57:32 -0500

    Understanding node methods

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+	{
+		LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+		LinkedListNode<int> headNode = myLList.First;
+		if (headNode == null)
+		{
+			myLList.AddFirst(newNode);
+			return newNode;
+		}
+		while (headNode.Next != null)
+		{
+			if (headNode.Value > n)
+			{
+				myLList.AddBefore(headNode, newNode);
+				return newNode;
+			}
+			headNode = headNode.Next;
+		}
+		myLList.AddLast(newNode);
+		return newNode;
+	}
+}
